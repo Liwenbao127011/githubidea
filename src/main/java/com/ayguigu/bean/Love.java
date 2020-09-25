e99c7ffc2717b8999ce523fc5a6a07fab3bfd2c7
李文宝 <3161208883@qq.com>
2020-09-25 16:29:44 +0800

    IDEA练习

---
+++ src/main/java/com/ayguigu/bean/Love.java
@@ -0,0 +1,12 @@
+package com.ayguigu.bean;
+
+/**
+ * @author Administrator
+ * @create 2020-09-25 16:23
+ */
+public class Love {
+
+    public static void main(String[] args) {
+        System.out.println("今天是什么日子.....");
+    }
+}
